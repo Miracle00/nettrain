2218b1b3b87f92a8bebd3cfb8a78b3f6748a7e57
SergenBAG <sergenbag01@gmail.com>
2019-02-11 15:46:47 +0300

    update footballApp and added note

--- entityframework/FootballApplication/FootballApplication/Migrations/Configuration.cs
+++ entityframework/FootballApplication/FootballApplication/Migrations/Configuration.cs
@@ -10,10 +10,19 @@ namespace FootballApplication.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            //True ise kontrol entityde, false ise kontrol kullanýcýda
         }
 
         protected override void Seed(FootballApplication.FootballContext context)
         {
+            //Add Or Update Komutunu kullanýrken Id vermek zorundayýz kayýt varsa güncelleyecek
+            //context.Teams.AddOrUpdate
+            //    (
+            //        new Team { Id = 1, Ad = "Galatasaray", KurulusYili = 1905 },
+            //        new Team { Id = 2, Ad = "Fenerbahçe", KurulusYili = 1907 },
+            //        new Team { Id = 3, Ad = "Trabzon", KurulusYili = 1967 }
+            //    );
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
