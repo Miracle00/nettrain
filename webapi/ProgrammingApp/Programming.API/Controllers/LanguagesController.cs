bc0371cc1a4bfe621d2142ab09468ed0e568b196
SergenBAG <sergenbag01@gmail.com>
2019-02-19 16:43:41 +0300

    added programming.dal and programming.api

---
+++ webapi/ProgrammingApp/Programming.API/Controllers/LanguagesController.cs
@@ -0,0 +1,35 @@
+﻿using Programming.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Programming.API.Controllers
+{
+    public class LanguagesController : ApiController
+    {
+        LanguagesDAL languageDAL = new LanguagesDAL();
+        public IEnumerable<Languages> Get()
+        {
+            return languageDAL.GetLanguages();
+        }
+        public Languages Get(int id)
+        {
+            return languageDAL.GetLanguagesById(id);
+        }
+        public Languages Post(Languages language)
+        {
+            return languageDAL.CreateLanguage(language);
+        }
+        public Languages Put(int id,Languages language)
+        {
+            return languageDAL.UpdateLanguage(id, language);
+        }
+        public void Delete(int id)
+        {
+            languageDAL.DeleteLanguage(id);
+        }
+    }
+}
---
+++ webapi/ProgrammingApp/Programming.DAL/LanguagesDAL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.DAL
+{
+    public class LanguagesDAL
+    {
+        ProgrammingDbEntities db = new ProgrammingDbEntities();
+
+        public IEnumerable<Languages> GetLanguages()
+        {
+            return db.Languages;
+        }
+        public Languages GetLanguagesById(int id)
+        {
+            return db.Languages.Find(id);
+        }
+        public Languages CreateLanguage(Languages language)
+        {
+            db.Languages.Add(language);
+            db.SaveChanges();
+            return language;
+        }
+        public Languages UpdateLanguage(int id,Languages language)
+        {
+            db.Entry(language).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return language;           
+        }
+        public void DeleteLanguage(int id)
+        {
+            db.Languages.Remove(db.Languages.Find(id));
+            db.SaveChanges();
+        }
+    }
+}
